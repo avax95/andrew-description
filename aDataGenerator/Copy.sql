0666310fcc73710878906bce2c52acbab42fd134
Andrew Liu <andrewliu@Andrews-MacBook-Pro-3.local>
2018-06-24 18:06:36 -0700

    created schema for cassandra

--- aDataGenerator/Copy.sql
+++ aDataGenerator/Copy.sql
@@ -1,4 +1,4 @@
-\COPY rooms(owner, "ownerPicture_Url", "propertyType", title, score, location, "numberOfGuest", "numberOfRooms", "numberBeds", "numberOfBaths", "numberOfViews", "descriptionSummary", smoking, "petSuitable", "partiesOrEvents", "noSafeForChildrenUnder", "checkInStartTime", "checkIntEndTime", "checkOutTime", "selfCheckInWithLockBox", rules, "rulestoAcknowledge", "cancellationType", "cancellationSummary", "nightsOfStayVary", "nightsOfMinimumStay", "daysFromLastUpdate") FROM '../heavySink/mainTable.csv' DELIMITER ',';
+-- \COPY rooms(owner, "ownerPicture_Url", "propertyType", title, score, location, "numberOfGuest", "numberOfRooms", "numberBeds", "numberOfBaths", "numberOfViews", "descriptionSummary", smoking, "petSuitable", "partiesOrEvents", "noSafeForChildrenUnder", "checkInStartTime", "checkIntEndTime", "checkOutTime", "selfCheckInWithLockBox", rules, "rulestoAcknowledge", "cancellationType", "cancellationSummary", "nightsOfStayVary", "nightsOfMinimumStay", "daysFromLastUpdate") FROM '../heavySink/mainTable.csv' DELIMITER ',';
 
 -- \COPY amenities(roomid, title, item, description) FROM '../heavySink/amenitiesTable.csv' DELIMITER ',';
 
