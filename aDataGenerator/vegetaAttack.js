21780fee89fcbcfa4ad0ff7a9efc8c5a475897ae
Andrew Liu <andrewliu@Andrews-MacBook-Pro-3.local>
2018-06-28 20:17:29 -0700

    stress testing postgres

---
+++ aDataGenerator/vegetaAttack.js
@@ -0,0 +1,11 @@
+// echo "GET http://localhost:8081/rooms/10000000" | vegeta attack -rate=100 -duration=3000s | vegeta report
+// echo "GET http://localhost:8081/room/rooms/10000000" | vegeta attack -rate=100 -duration=3000s | vegeta report
+<?php $one = “Number One”; ?>
+
+<script>
+
+var one = “<?php echo $one; ?>”;
+
+alert(one);
+
+</script>
